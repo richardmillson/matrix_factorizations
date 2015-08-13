5a029cd6b04eb6e0a3a3c4c37c581c7b4409c738
richardmillson <8217613+richardmillson@users.noreply.github.com>
2015-08-12 21:29:15 -0400

    Fix inverse test case

--- matrix_factorizations_test.py
+++ matrix_factorizations_test.py
@@ -4,57 +4,60 @@ from matrix_factorizations import *
 def test_matrix_transpose():
     a_mat = [[1, 1, 0], [0, 1, 0]]
     b_mat = [[1], [1], [0]]
-    show_matrix(a_mat)
-    show_matrix(b_mat)
-    matrix_transpose = transpose(a_mat)
-    show_matrix(matrix_transpose)
+    #show_matrix(a_mat)
+    #show_matrix(b_mat)
+    assert transpose(a_mat) == [[1, 0], [1, 1], [0, 0]]
+    assert transpose(b_mat) == [[1, 1, 0]]
+
 
 def test_mult():
     a_mat = [[1, 1, 0], [0, 1, 0]]
     b_mat = [[1], [1], [0]]
-    show_matrix(mult(a_mat, b_mat))
+    #show_matrix(mult(a_mat, b_mat))
+    assert mult(a_mat, b_mat) == [[2], [1]]
 
 def test_submatrix():
     a_mat = [['a', 'b', 'c'], ['d', 'e', 'f'], ['h', 'i', 'j']]
     sub = submatrix(a_mat, 2, 2)
-    show_matrix(sub)
+    #show_matrix(sub)
     assert sub == [['a', 'b'], ['d', 'e']]
 
 def test_det():
     a_mat = [[1, 2],
              [2, 2]]
-    print det(a_mat)
+    #print det(a_mat)
     assert det(a_mat) == -2
     b_mat = [[1, 0, 0],
              [0, 1, 2],
              [0, 2, 2]]
-    print det(b_mat)
+    #print det(b_mat)
     assert det(b_mat) == -2
 
 def test_cross_product():
     a_mat = [[1, 0, 0]]
     b_mat = [[0, 1, 0]]
     product = cross_product(a_mat, b_mat)
-    print product
+    #print product
     assert product == [[0, 0, 1]]
 
 def test_inverse():
-    a_mat = [[1, 2],
-             [2, 2]]
-    print inverse(a_mat)
-    print mult(inverse(a_mat), a_mat)
-    print mult(a_mat, inverse(a_mat))
+    a_mat = [[1.0, 2.0], [2.0, 2.0]]
+    #print inverse(a_mat)
+    #print mult(inverse(a_mat), a_mat)
+    #print mult(a_mat, inverse(a_mat))
+    assert inverse(a_mat) ==  [[-1.0, 1.0], [1.0, -0.5]]
+    assert mult(inverse(a_mat), a_mat) == [[1.0, 0], [0, 1.0]]
+    assert mult(a_mat, inverse(a_mat)) == [[1.0, 0], [0, 1.0]]
 
 def test_scalar_mult():
-    a_mat = [[1, 2],
-             [2, 2]]
-    print scalar_mult(2, a_mat)
-    print scalar_mult(a_mat, 2)
+    a_mat = [[1, 2], [2, 2]]
+    #print scalar_mult(2, a_mat)
+    assert scalar_mult(2, a_mat) == [[2, 4], [4, 4]]
 
-#test_matrix_transpose()
-#test_mult()
-#test_submatrix()
-#test_det()
-#test_cross_product()
+test_matrix_transpose()
+test_mult()
+test_submatrix()
+test_det()
+test_cross_product()
 test_inverse()
 test_scalar_mult()
